585f37bc96a499a7e0ea6bcd5c8d046dd9c2af40
ccssmania <ccss@utp.edu.co>
2018-11-20 13:06:12 +0100

    changin services page

--- resources/views/public/services.blade.php
+++ resources/views/public/services.blade.php
@@ -3,15 +3,14 @@
 <div class="container">
 
 	<!-- Page Heading/Breadcrumbs -->
-	<h1 class="mt-4 mb-3">Services
-		<small>Subheading</small>
+	<h1 class="mt-4 mb-3">Servicios
 	</h1>
 
 	<ol class="breadcrumb">
 		<li class="breadcrumb-item">
 			<a href="index.html">Home</a>
 		</li>
-		<li class="breadcrumb-item active">Services</li>
+		<li class="breadcrumb-item active">Servicios</li>
 	</ol>
 
 	<!-- Image Header -->
